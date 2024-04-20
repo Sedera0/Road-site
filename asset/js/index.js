2c68eac1618c49a4bc08118d2bc0171ad37eff31
Sedera RAFALIMANANA <hei.sedera@gmail.com>
2024-04-20 17:23:18 +0300

    Fix: Redirection and Sticky Navbar

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,8 +1,16 @@
+window.onload = function(){
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+    if (!isLoggedIn && window.location.pathname !== "/login.html") {
+        window.location.href = "login.html";
+    }
+};
+
+
 window.addEventListener("scroll", function(){
     const navbar = document.getElementById("navbar");
     const sticky = navbar.offsetTop;
 
-    if(window.scrollY > sticky){
+    if(window.scrollY > 50){
         navbar.classList.add("sticky")
     }else{
         navbar.classList.remove("sticky")
